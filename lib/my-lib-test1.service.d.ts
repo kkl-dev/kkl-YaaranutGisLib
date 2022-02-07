69ce47a408671923baf437c63199d0094325afec
uriokkl <88084814+uriokkl@users.noreply.github.com>
2022-02-07 10:35:07 +0200

    Delete lib directory

--- lib/my-lib-test1.service.d.ts
+++
@@ -1,6 +0,0 @@
-import * as i0 from "@angular/core";
-export declare class MyLibTest1Service {
-    constructor();
-    static ɵfac: i0.ɵɵFactoryDeclaration<MyLibTest1Service, never>;
-    static ɵprov: i0.ɵɵInjectableDeclaration<MyLibTest1Service>;
-}
